a841be1ceef445c3cd14f92bf3d56386596ae4d5
Jan Boesch <info@jan-boesch.net>
2020-08-25 23:16:09 +0200

    Ajout des étages

---
+++ plugins/digart/batiment/updates/builder_table_create_digart_batiment_etages.php
@@ -0,0 +1,28 @@
+<?php namespace Digart\Batiment\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateDigartBatimentEtages extends Migration
+{
+    public function up()
+    {
+        Schema::create('digart_batiment_etages', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('designation', 255);
+            $table->integer('sort_order')->nullable()->unsigned();
+            $table->integer('level')->nullable();
+            $table->boolean('is_actif')->nullable()->default(1);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('digart_batiment_etages');
+    }
+}
---
+++ plugins/digart/batiment/updates/seeder106.php
@@ -0,0 +1,41 @@
+<?php namespace Digart\Batiment\Updates;
+
+use Seeder;
+use Digart\Batiment\Models\Etage;
+
+class Seeder106 extends Seeder
+{
+    public function run()
+    {
+        
+        Etage::truncate();
+        
+        Etage::create([
+            'designation' => 'Sous-sol / cave',
+            'level' => -1,
+        ]); 
+        
+        Etage::create([
+            'designation' => 'Rez-de-chaussée',
+            'level' => '0',
+        ]); 
+        
+        Etage::create([
+            'designation' => '1er étage',
+            'level' => '1',
+        ]); 
+        
+        
+        Etage::create([
+            'designation' => '2e étage',
+            'level' => '2',
+        ]); 
+                    
+        Etage::create([
+            'designation' => '3e étage',
+            'level' => '3',
+        ]); 
+        
+
+    }
+}
